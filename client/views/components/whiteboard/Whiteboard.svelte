984ea38cf84fdde341087f4825468014c2cc2c6b
tsaxking <taylorking206@u.boisestate.edu>
2023-12-07 09:34:00 -0700

    merge button group

---
+++ client/views/components/whiteboard/Whiteboard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { FIRSTTeam } from "../../../models/FIRST/team";
+    import { Whiteboard } from "../../../models/whiteboard/whiteboard";
+
+    export let alliances: {
+        red: [FIRSTTeam | undefined, FIRSTTeam | undefined, FIRSTTeam | undefined],
+        blue: [FIRSTTeam | undefined, FIRSTTeam | undefined, FIRSTTeam | undefined]
+    } = {
+        red: [undefined, undefined, undefined],
+        blue: [undefined, undefined, undefined]
+    };
+
+    const canvas = document.getElementById('whiteboard') as HTMLCanvasElement;
+
+    const wb = new Whiteboard(canvas.getContext('2d') as CanvasRenderingContext2D);
+</script>
+
+
+<canvas id="whiteboard" width="1000" height="500"></canvas>
