cbcf7ad92921e5d050681baee810cfdf81e4009e
tsaxking <taylorking206@u.boisestate.edu>
2024-01-11 02:16:41 -0700

    merge

--- shared/db-types-extended.ts
+++ shared/db-types-extended.ts
@@ -12,7 +12,6 @@ export type Team = {
 
 export type CompLevel = 'pr' | 'qm' | 'ef' | 'qf' | 'sf' | 'f';
 
-
 export type Match = {
     id: string;
     eventKey: string;
@@ -33,17 +32,12 @@ export type CustomMatch = {
     blue1: number;
     blue2: number;
     blue3: number;
-}
-
-
+};
 
 export type ScoutingSection = {
     name: string;
     multiple: 0 | 1;
 };
-
-
-
 
 export type Whiteboard = {
     id: string;
@@ -52,8 +46,7 @@ export type Whiteboard = {
     board: string; // json array
     matchId?: string;
     customMatchId?: string;
-}
-
+};
 
 export type MatchScoutingComments = {
     id: string;
@@ -67,38 +60,45 @@ export type MatchScoutingComments = {
 export type ScoutingQuestionSection = {
     name: string;
     multiple: boolean;
-}
+};
 
 export type ScoutingQuestionGroup = {
     id: string;
     eventKey: string;
     section: string;
     name: string;
-}
+};
 
 export type ScoutingQuestion = {
     id: string;
     question: string;
-    type: 'text' | 'number' | 'boolean' | 'select' | 'checkbox' | 'radio' | 'textarea';
+    type:
+        | 'text'
+        | 'number'
+        | 'boolean'
+        | 'select'
+        | 'checkbox'
+        | 'radio'
+        | 'textarea';
     section: string;
     key: string;
     description: string;
     groupId: string;
-}
+};
 
 export type ScoutingAnswer = {
     id: string;
     questionId: string;
     answer: string;
     teamNumber: number;
-}
+};
 
 export type TBARequest = {
     url: string;
     response: string;
     updated: number;
     update: 0 | 1;
-}
+};
 
 export type Checklist = {
     id: string;
@@ -112,19 +112,19 @@ export type ChecklistQuestion = {
     checklistId: string;
     question: string;
     interval: number;
-}
+};
 
 export type CheckListAssignment = {
     checklistId: string;
     accountId: string;
-}
+};
 
 export type ChecklistAnswer = {
     id: string;
     accountId: string;
     questionId: string;
     matchId: string;
-}
+};
 
 export type Alliance = {
     id: string;
@@ -133,7 +133,7 @@ export type Alliance = {
     team1: number;
     team2: number;
     team3: number;
-}
+};
 
 export type Strategy = {
     id: string;
@@ -144,8 +144,7 @@ export type Strategy = {
     matchId: string | null;
     customMatchId: string | null;
     comment: string;
-}
-
+};
 
 export type MatchScouting = {
     id: string;
@@ -155,7 +154,7 @@ export type MatchScouting = {
     scoutGroup: string;
     time: number;
     preScouting: 0 | 1;
-    trace: string // json array
+    trace: string; // json array
 };
 
 export type RetrievedMatchScouting = {
@@ -170,7 +169,7 @@ export type RetrievedMatchScouting = {
     eventKey: string;
     matchNumber: number;
     compLevel: string;
-}
+};
 
 export type RetrievedScoutingAnswer = {
     id: string;
@@ -180,12 +179,19 @@ export type RetrievedScoutingAnswer = {
     question: string;
     questionKey: string;
     questionDescription: string;
-    questionType: 'text' | 'number' | 'boolean' | 'select' | 'checkbox' | 'radio' | 'textarea';
+    questionType:
+        | 'text'
+        | 'number'
+        | 'boolean'
+        | 'select'
+        | 'checkbox'
+        | 'radio'
+        | 'textarea';
     groupId: string;
     eventKey: string;
     section: string;
     groupName: string;
-}
+};
 
 export type DiscordPair = {
     key: string;
