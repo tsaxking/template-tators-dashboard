635ea6d2b19cda7b80a68384215029e59de50c42
tsaxking <taylorking206@u.boisestate.edu>
2024-01-10 21:25:18 -0700

    approved!

--- client/entries/test/fuzzy.ts
+++ client/entries/test/fuzzy.ts
@@ -1,10 +1,10 @@
 import './../../utilities/imports';
-import FuzzySearch from '../../views/components/FuzzySearch.svelte'
+import FuzzySearch from '../../views/components/FuzzySearch.svelte';
 const app = new FuzzySearch({
     target: document.body,
     props: {
         searchOptions: [
-           `Ex mollit ut cillum aute cillum qui tempor veniam voluptate et Lorem. Nisi cupidatat duis magna eu dolore aliquip consequat enim. Nostrud excepteur nostrud reprehenderit qui aute.
+            `Ex mollit ut cillum aute cillum qui tempor veniam voluptate et Lorem. Nisi cupidatat duis magna eu dolore aliquip consequat enim. Nostrud excepteur nostrud reprehenderit qui aute.
 
 Magna sint deserunt amet cupidatat. Incididunt id veniam id dolore cillum commodo aliquip consectetur consectetur minim ea nisi eu et. Culpa anim Lorem id exercitation.
 
@@ -17,27 +17,27 @@ Ullamco deserunt sint velit pariatur elit deserunt exercitation nisi laboris cil
 Irure reprehenderit elit excepteur sint irure est officia officia quis. Consequat do labore nulla magna do cillum cupidatat deserunt nostrud commodo. Commodo aliqua occaecat occaecat nisi consectetur id id proident pariatur adipisicing cillum aliqua eiusmod consequat. Nulla cupidatat ullamco amet aute pariatur proident ipsum sint ut laborum. Cupidatat dolor incididunt qui duis ex eiusmod do nostrud commodo quis pariatur.
 
 Cillum cillum ad qui elit proident officia anim id nisi mollit. Mollit esse veniam esse nostrud sit amet dolore minim officia culpa occaecat veniam fugiat. Sunt excepteur reprehenderit officia minim ea incididunt ut sit excepteur consequat minim tempor irure aliquip. In sunt velit Lorem excepteur nostrud irure nisi. Aliquip laborum aute esse aute laboris dolor eu elit dolore nisi et sint occaecat.`,
-`Exercitation Lorem excepteur laboris do non ex laboris est anim laboris cillum excepteur sint. Amet ex nisi occaecat enim nostrud dolor amet aliqua quis. Exercitation duis dolore tempor amet minim ullamco excepteur id cupidatat est enim nulla.
+            `Exercitation Lorem excepteur laboris do non ex laboris est anim laboris cillum excepteur sint. Amet ex nisi occaecat enim nostrud dolor amet aliqua quis. Exercitation duis dolore tempor amet minim ullamco excepteur id cupidatat est enim nulla.
 
 Nisi voluptate proident culpa amet do sint veniam quis nulla proident. Ex ipsum Lorem sint deserunt magna non enim culpa ad magna velit quis. Excepteur tempor qui cupidatat nisi labore ut ex irure ut ad enim ipsum incididunt cupidatat.
 
 Laboris eu consectetur esse commodo. Qui laborum ex irure incididunt nulla. Proident incididunt anim ea sunt velit ipsum enim enim sint ut eu.
 
 Ea commodo minim dolor culpa culpa ut dolor reprehenderit nostrud cillum cupidatat. Ea veniam ullamco cupidatat aute aliquip labore. Exercitation esse sunt nulla dolor enim ea. Excepteur laborum quis aute ut dolor.`,
-`Ut incididunt consectetur irure consequat veniam et tempor esse nisi magna. Ex sit excepteur do sit. Cupidatat exercitation culpa aute nisi occaecat consequat consectetur amet. Pariatur aliqua nulla id nostrud dolor Lorem laborum nisi.
+            `Ut incididunt consectetur irure consequat veniam et tempor esse nisi magna. Ex sit excepteur do sit. Cupidatat exercitation culpa aute nisi occaecat consequat consectetur amet. Pariatur aliqua nulla id nostrud dolor Lorem laborum nisi.
 
 Et Lorem ad deserunt officia laborum. Pariatur laboris consequat minim enim irure esse ipsum occaecat culpa eu velit. Minim enim ea elit non velit duis minim ex.
 
 Aliqua aliqua ullamco cillum dolor elit consectetur do cillum ipsum Lorem. Tempor exercitation minim do deserunt ut velit irure occaecat ea. Exercitation tempor Lorem pariatur tempor minim Lorem est. Dolor et ex sit aute. Amet veniam enim incididunt exercitation non qui magna quis consequat. Dolor dolore aute aute aliquip ad consectetur commodo cupidatat occaecat irure aliqua sit irure proident.
 
 Incididunt dolore cupidatat tempor commodo anim enim nulla. Lorem enim consequat adipisicing velit in excepteur commodo eiusmod magna pariatur proident Lorem ea. Consequat incididunt ea culpa enim consectetur fugiat sit in dolore incididunt cillum labore sunt. Pariatur aliquip eiusmod pariatur cillum. Anim ex elit magna in incididunt. Aliquip incididunt laborum aliquip excepteur officia.`,
-`Labore occaecat anim duis anim amet do nisi mollit officia ut ad labore. Fugiat laborum incididunt nisi aliqua aliqua ea amet nisi quis Lorem sit occaecat. Do enim cillum labore laborum. Enim velit officia qui ad duis officia ut deserunt esse velit labore et ea laborum. Nostrud minim deserunt aute ad aliqua incididunt. Nostrud do occaecat cupidatat anim occaecat ut. Labore eiusmod qui ex reprehenderit minim sunt.
+            `Labore occaecat anim duis anim amet do nisi mollit officia ut ad labore. Fugiat laborum incididunt nisi aliqua aliqua ea amet nisi quis Lorem sit occaecat. Do enim cillum labore laborum. Enim velit officia qui ad duis officia ut deserunt esse velit labore et ea laborum. Nostrud minim deserunt aute ad aliqua incididunt. Nostrud do occaecat cupidatat anim occaecat ut. Labore eiusmod qui ex reprehenderit minim sunt.
 
 Ipsum occaecat consectetur laboris aliqua do exercitation laborum ut culpa fugiat non magna deserunt ad. Consectetur officia ut anim minim elit. Reprehenderit dolore velit ut anim eiusmod. Fugiat velit minim consectetur commodo ea enim veniam culpa. Elit eu consectetur laborum incididunt. Amet culpa mollit ea qui id veniam labore voluptate.
 
 Cupidatat incididunt proident ex nisi nulla ut ipsum ut cupidatat nulla reprehenderit. Eiusmod minim excepteur Lorem aliqua commodo aliquip ullamco. Anim qui ex labore non aute duis in. Esse consectetur cillum ad officia cillum ipsum adipisicing amet nostrud amet dolor aliqua veniam exercitation. Ullamco ad id ex mollit aute mollit nostrud cupidatat minim. Minim labore excepteur commodo consectetur commodo commodo.
 
-Minim tempor excepteur et excepteur nulla elit duis minim magna aliqua dolore mollit. Elit duis Lorem eu aliqua. Nisi ea exercitation excepteur ea voluptate velit.` 
+Minim tempor excepteur et excepteur nulla elit duis minim magna aliqua dolore mollit. Elit duis Lorem eu aliqua. Nisi ea exercitation excepteur ea voluptate velit.`
         ]
     }
-})
+});
