7dd11412e822966d1084d08852195603f7ab8912
Taylor King <taylorking206@u.boisestate.edu>
2024-01-15 15:56:11 -0700

    change trace structure

--- shared/trace.ts
+++ shared/trace.ts
@@ -1,11 +1,7 @@
-// export type Point = [number, number, number]; // x, y, time (normalized to 0-1)
+export type Actions = 'spk' | 'amp' | 'src' | 'trp' | 'clb';
+type P = [number, number, number, Actions | undefined];
+type TraceArray = P[];
 
-// export type Action = {
-//     name: string;
-//     point: Point;
-// };
-
-// export type Trace = (Point | Action)[];
 
 /**
  * Description placeholder
@@ -16,42 +12,59 @@
 const chars =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}|:"<>?`~[]\';./=\\,';
 
-/**
- * Description placeholder
- * @date 1/11/2024 - 3:10:27 AM
- */
-const compress = (str: string) => {
-    let num = +str;
-    const base = chars.length;
+export class Trace {
+    // static encode(trace: TraceArray) {
+    //     return trace.map(Trace.compress);
+    // }
+    // static decode(trace: TraceArray) {
+    //     return trace.map(Trace.decompress);
+    // }
 
-    let result = '';
-    while (num > 0) {
-        result = chars[num % base] + result;
-        num = Math.floor(num / base);
+    // static compress(point: P): string {
+    //     const [i, x, y, a] = point;
+    //     // compress the point to a smaller string
+
+
+
+    // }
+
+    // static decompress(str: string) {}
+
+    constructor(public readonly trace: TraceArray) {}
+
+
+
+    velocityMap() {
+        return this.trace.map((p1, i, a) => {
+            if (i === a.length - 1) return null;
+
+            const [,x1,y1] = p1;
+            const [,x2,y2] = a[i + 1];
+
+            const dx = x2 - x1;
+            const dy = y2 - y1;
+            
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            return distance / .25;
+        }).filter((p) => p !== null);
     }
-    return result;
-};
 
-/**
- * Description placeholder
- * @date 1/11/2024 - 3:10:27 AM
- */
-const decompress = (str: string) => {
-    let num = 0;
-    const base = chars.length;
-    for (let i = 0; i < str.length; i++) {
-        num = num * base + chars.indexOf(str[i]);
+    velocityHistogram() {
+        const map = this.velocityMap();
+
+        const max = Math.max(...map);
+        const min = Math.min(...map);
+
+        const range = max - min;
+
+        const buckets = new Array(10).fill(0);
+
+        map.forEach((v) => {
+            const bucket = Math.floor(((v - min) / range) * 10);
+            buckets[bucket]++;
+        });
+
+        return buckets;
     }
-    return num;
 };
-
-/**
- * Description placeholder
- * @date 1/11/2024 - 3:10:27 AM
- */
-export const encode = (trace: string[]) => trace.map(compress);
-/**
- * Description placeholder
- * @date 1/11/2024 - 3:10:27 AM
- */
-export const decode = (trace: string[]) => trace.map(decompress);
