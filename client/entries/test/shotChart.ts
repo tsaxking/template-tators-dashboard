78efd5b540adbcb3f69f73cb0f07e5efecd6ccff
Sambews <ton.of.keys@gmail.com>
2024-01-22 10:05:02 -0700

    Got elements to appear

--- client/entries/test/shotChart.ts
+++ client/entries/test/shotChart.ts
@@ -3,9 +3,11 @@ import { generateTrace } from '../../../shared/dummy-data';
 import {
     P,
     Trace,
-} from '../../../shared/submodules/tatorscout-calculations/trace';
+} 
+from '../../../shared/submodules/tatorscout-calculations/trace';
 import { Canvas } from '../../models/canvas/canvas';
 import { Circle } from '../../models/canvas/circle';
+import 'jquery-ui';
 
 const shotCanvas = document.createElement('canvas');
 const ctx = shotCanvas.getContext('2d');
@@ -13,11 +15,14 @@ if (!ctx) throw new Error('Could not get canvas context');
 const c = new Canvas(ctx);
 
 document.body.append(shotCanvas);
+$(document.body).append($(create('button', {label: "custom label"})));
 
 c.clear();
+
 c.add(
     ...generateTrace()
-        .filter(Trace.filterAction('spk'))
-        .map((p) => new Circle([p[1], p[2]], 0.05)),
+        .filter(Trace.filterAction('spk')).filter(Trace.filterIndex(0, 600))
+        .map((p) => new Circle([p[1], p[2]], 0.05))
 );
+
 c.draw();
