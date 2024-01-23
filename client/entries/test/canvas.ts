e852aaa02f67d79dfdebface06d03a09471f6eb4
tsaxking <taylorking206@u.boisestate.edu>
2024-01-22 21:55:16 -0700

    init canvas

---
+++ client/entries/test/canvas.ts
@@ -0,0 +1,7 @@
+import './../../utilities/imports';
+
+import Canvas from '../../views/components/Canvas.svelte';
+
+new Canvas({
+    target: document.body,
+});
--- client/models/canvas/container.ts
+++ client/models/canvas/container.ts
@@ -3,24 +3,24 @@ import { Drawable } from "./canvas";
 
 
 export class Container implements Drawable {
-    private $elements: Drawable[] = [];
+    private $elements: (Drawable | null)[] = [];
     private $filtered: Drawable[] = [];
 
-    constructor(...elements: Drawable[]) {
+    constructor(...elements: (Drawable | null)[]) {
         this.elements = elements;
     }
 
-    public set elements(value: Drawable[]) {
+    public set elements(value: (Drawable | null)[]) {
         this.$elements = value;
-        this.$filtered = value;
+        this.$filtered = value.filter((e): e is Drawable => e !== null);
     }
 
-    public get elements(): Drawable[] {
+    public get elements(): (Drawable | null)[] {
         return this.$elements;
     }
 
-    filter(fn: (element: Drawable) => boolean): void {
-        this.$filtered = this.$elements.filter(fn);
+    filter(fn: (element: Drawable | null, i: number, arr: (Drawable | null)[]) => boolean): void {
+        this.$filtered = this.$elements.filter(fn).filter((e): e is Drawable => e !== null);
     }
 
     sort(fn: (a: Drawable, b: Drawable) => number): void {
