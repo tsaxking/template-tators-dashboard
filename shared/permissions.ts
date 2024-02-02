36592be807b063ea00196d4e48730f18059a2260
tsaxking <taylorking206@u.boisestate.edu>
2024-02-02 03:22:19 -0700

    scouting question permissions

--- shared/permissions.ts
+++ shared/permissions.ts
@@ -1,23 +1,2 @@
-export type Permission =
-    | 'editRoles'
-    | 'admin'
-    | 'viewData'
-    | 'editDatabase'
-    | 'editPermissions'
-    | 'editRoles'
-    | 'editUsers'
-    | 'viewLogs'
-    | 'verify'
-    | 'editScoutingQuestions'
-    | 'settings'
-    | 'logs'
-    | 'verify'
-    | 'editUsers';
-export type RoleName =
-    | 'admin'
-    | 'admin'
-    | 'mentor'
-    | 'developer'
-    | 'scout'
-    | 'viewer'
-    | 'user';
+export type Permission = 'editRoles' | 'admin' | 'viewData' | 'editDatabase' | 'editPermissions' | 'editRoles' | 'editUsers' | 'viewLogs' | 'verify' | 'editScoutingQuestions' | 'settings' | 'logs' | 'verify' | 'editUsers' | 'submitScoutingAnswers';
+export type RoleName = 'admin' | 'admin' | 'mentor' | 'developer' | 'scout' | 'viewer' | 'user';
