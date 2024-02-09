ccb1700df49104f2bd3784e673074016bc50c71a
tsaxking <taylorking206@u.boisestate.edu>
2024-02-08 17:10:58 -0700

    options work

--- client/views/pages/edit-pit-scouting/Option.svelte
+++ client/views/pages/edit-pit-scouting/Option.svelte
@@ -1,24 +1,26 @@
 <script lang="ts">
+import { createEventDispatcher } from 'svelte';
+
+const id = Math.random().toString(36).substring(2);
 export let text: string = '';
+
+const dispatch = createEventDispatcher();
 </script>
 
-<div class="card">
-    <div class="card-body">
-        <div class="d-flex justify-content-between">
-            <button class="btn btn-danger">
-                <i class="material-icons">close</i>
-            </button>
-        </div>
-        <div class="container">
-            <div class="row mb-3">
-                <label for="text">Option Text</label>
-                <input
-                    type="text"
-                    class="form-control"
-                    id="text"
-                    bind:value="{text}"
-                />
-            </div>
+<div class="col-lg-4 col-md-6 col-sm-12 p-0 m-0">
+    <div class="d-inline-flex">
+        <button class="btn btn-danger h-75 my-auto" on:click="{() => dispatch('delete')}">
+            <i class="material-icons">close</i>
+        </button>
+        <div class="form-floating">
+            <input
+                type="text"
+                class="form-control"
+                id="text-{id}"
+                bind:value="{text}"
+                on:change="{() => dispatch('update', { id, text })}"
+            />
+            <label for="text-{id}">Option Text</label>
         </div>
     </div>
 </div>
