e7a8e8950216a103730418d187194fef7af02706
Taylor King <taylorking206@u.boisestate.edu>
2024-02-11 20:09:03 -0700

    picture upload

---
+++ client/views/components/robot-display/UploadTeamPicture.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import UploadPicture from "../main/UploadPicture.svelte";
+import Modal from "../bootstrap/Modal.svelte";
+    import { FIRSTTeam } from '../../../models/FIRST/team';    
+
+    export let team: FIRSTTeam;
+    let fileList: FileList;
+
+</script>
+
+<Modal title="Upload {team.number} picture">
+    <UploadPicture multiple={true} on:change={({ detail: { fileList } }) => {
+        fileList = fileList;
+    }}/>
+    <div slot="buttons">
+        <button type="button" class="btn btn-primary" on:click={() => {
+            if (!team) return;
+            team.savePictures(fileList);
+        }}>Save</button>
+    </div>
+</Modal>
+
