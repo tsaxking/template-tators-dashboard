3b1aa29b07a4a76e7bf041434e14fbad049482b0
Taylor King <taylorking206@u.boisestate.edu>
2024-02-14 13:40:20 -0700

    pull events

--- scripts/manager/server-controller.ts
+++ scripts/manager/server-controller.ts
@@ -1,9 +1,8 @@
-import { DB } from '../../server/utilities/databases.ts';
 import { TBA } from '../../server/utilities/tba/tba.ts';
-import { uuid } from '../../server/utilities/uuid.ts';
-import { TBAEvent, TBATeam, TBAMatch } from '../../shared/submodules/tatorscout-calculations/tba.ts';
+import { TBAEvent } from '../../shared/submodules/tatorscout-calculations/tba.ts';
 import { backToMain } from '../manager.ts';
 import { select } from '../prompt.ts';
+import { pullEvent } from '../../server/utilities/tba/pull-event.ts';
 
 export const pullEvents = async () => {
     const events = await TBA.get<TBAEvent[]>(`/team/frc2122/events/${new Date().getFullYear()}`)
@@ -23,78 +22,9 @@ export const pullEvents = async () => {
 
         if (!event) return backToMain('No event selected');
 
-        const hasEvent = await DB.get('events/from-key', {
-            eventKey: event.key
-        });
-
-        if (hasEvent.isErr()) {
-            return backToMain('Error checking for event: ' + hasEvent.error);
-        }
-
-        if (!hasEvent.value) {
-            DB.run('events/new-event', {
-                eventKey: event.key,
-                flipX: 0,
-                flipY: 0
-            });
-        }
-
-        const [teams, matches] = await Promise.all([
-            TBA.get<TBATeam[]>(`/event/${event.key}/teams`),
-            TBA.get<TBAMatch[]>(`/event/${event.key}/matches`)
-        ]);
-
-        if (teams.isErr()) {
-            return backToMain('Error retrieving teams: ' + teams.error);
-        }
-
-        if (matches.isErr()) {
-            return backToMain('Error retrieving matches: ' + matches.error);
-        }
-
-        if (teams.value) {
-            const current = await DB.all('teams/from-event', {
-                eventKey: event.key
-            });
-
-            if (current.isErr()) {
-                return backToMain('Error retrieving current teams: ' + current.error);
-            }
-
-            for (const team of teams.value) {
-                const has = current.value.find(t => t.number === team.team_number);
-                if (!has) {
-                    DB.run('teams/new', {
-                        eventKey: event.key,
-                        number: team.team_number,
-                        watchPriority: 0
-                    });
-                }
-            }
-        }
-
-        if (matches.value) {
-            const current = await DB.all('matches/from-event', {
-                eventKey: event.key
-            });
-
-            if (current.isErr()) {
-                return backToMain('Error retrieving current matches: ' + current.error);
-            }
-
-            for (const match of matches.value) {
-                const has = current.value.find(m => m.matchNumber === match.match_number && m.compLevel === match.comp_level);
-
-                if (!has) {
-                    DB.run('matches/new', {
-                        eventKey: event.key,
-                        matchNumber: match.match_number,
-                        compLevel: match.comp_level,
-                        id: uuid()
-                    });
-                }
-            }
-        }
+        const res = await pullEvent(event.key);
+        if (res.isErr()) return backToMain('Error pulling event: ' + res.error);
+        return backToMain('Event pulled successfully');
     } else {
         return backToMain('Error retrieving events: ' + events.error);
     }
