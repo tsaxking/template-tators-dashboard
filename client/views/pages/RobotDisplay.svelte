6ac52d426c73e0778bb07ea1fbad5e47ebd8df38
tsaxking <taylorking206@u.boisestate.edu>
2024-02-28 01:28:52 -0700

    container-fluid

--- client/views/pages/RobotDisplay.svelte
+++ client/views/pages/RobotDisplay.svelte
@@ -35,7 +35,7 @@ $: fns.getTeam(team);
 FIRSTTeam.on('select', (t: FIRSTTeam) => (team = t));
 </script>
 
-<div class="container">
+<div class="container-fluid">
     <div class="row mb-3">
         <div class="col-md-6">
             <RobotSelect></RobotSelect>
