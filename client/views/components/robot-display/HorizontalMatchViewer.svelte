a215e9ef7f37a762f71c3f43a4131ce7a21e7085
tsaxking <taylorking206@u.boisestate.edu>
2024-03-11 15:13:02 -0600

    finish

--- client/views/components/robot-display/HorizontalMatchViewer.svelte
+++ client/views/components/robot-display/HorizontalMatchViewer.svelte
@@ -1,1 +1,32 @@
-<script lang="ts"></script>
+<script lang="ts">
+import { FIRSTTeam } from "../../../models/FIRST/team";
+import { MatchScouting } from "../../../models/FIRST/match-scouting";
+import MatchViewer from "./MatchViewer.svelte";
+
+
+export let team: FIRSTTeam | undefined = undefined;
+export let matches: MatchScouting[] = [];
+
+
+const fns = {
+    getTeam: async (t?: FIRSTTeam) => {
+        if (!t) return;
+        const matchesRes = await t.getMatchScouting();
+        if (matchesRes.isOk()) {
+            matches = matchesRes.value;
+        }
+    }
+}
+
+$: fns.getTeam(team);
+</script>
+
+<div class="d-flex scroll-x">
+    {#if !!team}
+        {#each matches as match}
+            <div class="mx-5">
+                <MatchViewer {team} {match} />
+            </div>
+        {/each}
+    {/if}
+</div>
