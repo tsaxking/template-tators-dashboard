0cc99b19bb4daa5909b2e54cbed8ba5ba2d20628
Taylor King <taylorking206@u.boisestate.edu>
2024-03-11 21:17:46 -0600

    cors

--- server/structure/app/app.ts
+++ server/structure/app/app.ts
@@ -8,6 +8,7 @@ import http from 'http';
 import { Server } from 'socket.io';
 import { Session } from '../sessions';
 import session from 'express-session';
+import cors from 'cors';
 
 /**
  * All file types that can be sent (can be expanded)
@@ -357,6 +358,10 @@ export class App<sessionInfo = unknown> {
 
         this.server.use(express.json());
         this.server.use(express.urlencoded({ extended: true }));
+        this.server.use(cors({
+            origin: '*',
+            credentials: true,
+        }));
         // this.server.use(
         //     session({
         //         secret: 'hello darkness my old friend',
