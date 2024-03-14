bb4412e2c11918161d64458cbe14bb737de18fcc
Taylor King <taylorking206@u.boisestate.edu>
2024-03-14 10:55:17 -0600

    filter matches

--- client/models/FIRST/team.ts
+++ client/models/FIRST/team.ts
@@ -240,8 +240,10 @@ export class FIRSTTeam extends Cache<FIRSTTeamEventData> {
 
             if (res.isErr()) throw res.error;
 
-            this.$cache.set('match-scouting', res.value);
-            return res.value;
+            const data = res.value.filter((s, i, a) => a.findIndex(_s => _s.id === s.id) === i);
+
+            this.$cache.set('match-scouting', data);
+            return data;
         });
     }
 
@@ -352,7 +354,7 @@ export class FIRSTTeam extends Cache<FIRSTTeamEventData> {
             );
 
             if (res.isErr()) throw res.error;
-            return res.value;
+            return res.value.filter((s, i, a) => a.findIndex(_s => _s.id === s.id) === i);
         });
     }
 
