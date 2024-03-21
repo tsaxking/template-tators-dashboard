7f4e9e311d2697ba31c749825928e2d5851d55e7
tsaxking <taylorking206@u.boisestate.edu>
2024-03-20 20:01:25 -0600

    event summary

--- client/views/pages/EventSummary.svelte
+++ client/views/pages/EventSummary.svelte
@@ -7,7 +7,8 @@ import { Color } from "../../submodules/colors/color";
     export let event: FIRSTEvent;
 
     let data: {
-        name: string[];
+        title: string;
+        labels: string[];
         data: {
             [key: number]: number[];
         };
@@ -28,32 +29,47 @@ import { Color } from "../../submodules/colors/color";
 
 <div class="container-fluid">
     {#each data as row}
-        <div class="row">
-            <div class="col-12">
-                <Bar 
-                    data={{
-                        labels: Object.keys(row.data),
-                        datasets: row.name.map((name, i) => {
-                            const c = Color.random();
-                            return {
-                                label: name,
-                                data: Object.values(row.data).map((v) => v[i]),
-                                backgroundColor: c.setAlpha(0.2).toString('rgba'),
-                                borderColor: c.setAlpha(1).toString('rgba'),
-                                borderWidth: 1
-                            }
-                        })
-                    }}
+        <div class="row mb-3">
+            <div class="card p-0">
+                <div class="card-header">
+                    <div class="card-title">
+                        {row.title}
+                    </div>
+                </div>
+                <div class="card-body">
+                    <Bar 
+                        data={{
+                            labels: Object.keys(row.data),
+                            datasets: row.labels.map((label, i) => {
+                                const c = Color.random();
+                                return {
+                                    label,
+                                    data: Object.values(row.data).map((v) => v[i]),
+                                    backgroundColor: c.setAlpha(0.2).toString('rgba'),
+                                    borderColor: c.setAlpha(1).toString('rgba'),
+                                    borderWidth: 1
+                                }
+                            })
+                        }}
 
-                    options={{
-                        scales: {
-                            y: {
-                                beginAtZero: true
-                            }
-                        }
-                    }}
-                />
+                        options={{
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            },
+                            responsive: true,
+                            maintainAspectRatio: false
+                        }}
+                    />
+                </div>
             </div>
         </div>
     {/each}
 </div>
+
+<style>
+    .card-body {
+        height: 400px;
+    }
+</style>
