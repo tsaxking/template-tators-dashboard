5425f95ff4da1d8ac70ff7ec731920070ca8cd0d
tsaxking <taylorking206@u.boisestate.edu>
2024-03-22 11:04:50 -0600

    log error on summary

--- server/routes/api/events.ts
+++ server/routes/api/events.ts
@@ -141,10 +141,10 @@ router.post<{
         TBA.get<TBAMatch[]>('/event/' + eventKey + '/matches')
     ]);
 
-    if (matchScouting.isErr()) return res.sendStatus('unknown:error');
-    if (teamsResult.isErr()) return res.sendStatus('unknown:error');
+    if (matchScouting.isErr()) {console.error(matchScouting.error); return res.sendStatus('unknown:error');}
+    if (teamsResult.isErr()) {console.error(teamsResult.error); return res.sendStatus('unknown:error');}
     if (!teamsResult.value) return res.sendStatus('tba:invalid-path');
-    if (matchesResult.isErr()) return res.sendStatus('unknown:error');
+    if (matchesResult.isErr()) {console.error(matchesResult.error); return res.sendStatus('unknown:error');}
     if (!matchesResult.value) return res.sendStatus('tba:invalid-path');
 
     const matches = matchesResult.value.sort(matchSort);
