49f3871846dedaa956d313781ddb7e702e2fc052
Tator Scout <tatorscout@gmail.com>
2024-04-01 19:33:44 -0700

    Made Github link/icon a secondary color

--- client/views/components/main/Footer.svelte
+++ client/views/components/main/Footer.svelte
@@ -34,7 +34,7 @@ const quote = Random.choose(quotes);
 </script>
 
 <footer>
-    <p class="text-muted text-center text-reset" >
+    <p class="text-muted text-center link-secondary" >
         &copy; {new Date().getFullYear()}
         {domain} | All Rights Reserved
         <br />
