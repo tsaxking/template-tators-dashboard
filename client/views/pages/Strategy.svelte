6e694e915c17422208afd28d4740b19e8f11d4e3
tsaxking <taylorking206@u.boisestate.edu>
2024-04-17 15:52:39 -0600

    complete

--- client/views/pages/Strategy.svelte
+++ client/views/pages/Strategy.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
-import AllianceComparison from '../components/strategy/AllianceComparison.svelte';
+import AllianceComparison from '../components/strategy/2024Alliance.svelte';
 </script>
-
-<AllianceComparison />
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-6">
+            <AllianceComparison color="red" />
+        </div>
+        <div class="col-md-6">
+            <AllianceComparison color="blue" />
+        </div>
+    </div>
+</div>
