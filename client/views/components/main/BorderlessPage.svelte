e26239292d3a81eac269d334b6e88c629db3a6b8
Raygen Rupe <raygenrrupe@gmail.com>
2024-06-17 16:01:43 -0600

    rudimentary pit dashboard, basic borderless pages

---
+++ client/views/components/main/BorderlessPage.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { Random } from '../../../../shared/math';
+import { capitalize, fromSnakeCase } from '../../../../shared/text';
+import Footer from './Footer.svelte';
+export let title: string;
+let year: number = new Date().getFullYear();
+export let domain: string;
+export let active: string;
+</script>
+
+{#if active === title}
+    <div class="container-fluid m-0">
+        <slot />
+    </div>
+{/if}
+
+<style>
+.m-0 {
+    margin: 0;
+    padding: 0;
+}
+</style>
