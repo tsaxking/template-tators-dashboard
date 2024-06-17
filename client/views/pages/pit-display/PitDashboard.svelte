c425b5c18374b91a7d78d930d9cf0872869caae6
Raygen Rupe <raygenrrupe@gmail.com>
2024-06-17 16:58:09 -0600

    more basic structure, fix scroll

--- client/views/pages/pit-display/PitDashboard.svelte
+++ client/views/pages/pit-display/PitDashboard.svelte
@@ -1,22 +1,58 @@
 <script lang="ts">
 </script>
 
-<div class="vh-100 vw-100 container-fluid">
+<div class="vh-display vw-100 container-fluid position-relative">
     <div class="row h-100">
         <div class="col-md-6 bg-danger">
-            <!-- Left container -->
+            <div
+                class="d-flex flex-column h-100 justify-content-between align-items-start p-3"
+            >
+                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
+                    <!-- Content 1 -->
+                </div>
+                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
+                    <!-- Content 2 -->
+                </div>
+                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
+                    <!-- Content 3 -->
+                </div>
+            </div>
         </div>
         <div class="col-md-6 bg-primary">
-            <!-- Right container -->
+            <div
+                class="d-flex flex-column h-100 justify-content-between align-items-end p-3"
+            >
+                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
+                    <!-- Content 1 -->
+                </div>
+                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
+                    <!-- Content 2 -->
+                </div>
+                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
+                    <!-- Content 3 -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Dark gray square overlay -->
+    <div
+        class="container h-25 w-25 bg-gray-light rounded position-absolute top-50 start-50 translate-middle z-3"
+    >
+        <div
+            class="d-flex flex-column justify-content-center align-items-center h-100 my-2"
+        >
+            <h4 class="text-black">Next match is in __ minutes at</h4>
+            <p class="display-1 text-black" style="font-size:900%">00:00</p>
         </div>
     </div>
 </div>
 
 <style>
-.bg-red {
-    background-color: red;
+.bg-gray-light {
+    background-color: rgba(200, 200, 200, 0.7);
 }
-.bg-blue {
-    background-color: blue;
+
+.vh-display {
+    height: calc(100vh - 58px);
 }
 </style>
