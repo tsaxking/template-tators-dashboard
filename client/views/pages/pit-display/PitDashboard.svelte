720d320c851cc7018eb5f173c087c2d57ee39f78
Raygen Rupe <raygenrrupe@gmail.com>
2024-06-17 16:01:43 -0600

    rudimentary pit dashboard, basic borderless pages

---
+++ client/views/pages/pit-display/PitDashboard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<div class="vh-100 vw-100 container-fluid">
+    <div class="row h-100">
+        <div class="col-md-6 bg-danger">
+            <!-- Left container -->
+        </div>
+        <div class="col-md-6 bg-primary">
+            <!-- Right container -->
+        </div>
+    </div>
+</div>
+
+<style>
+.bg-red {
+    background-color: red;
+}
+.bg-blue {
+    background-color: blue;
+}
+</style>
