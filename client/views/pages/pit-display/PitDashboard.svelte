bc93f81a08cfaacecb66742ace89c8c48c12edb3
Raygen Rupe <raygenrrupe@gmail.com>
2024-06-17 20:14:54 -0600

    rudimentary timing stuff

--- client/views/pages/pit-display/PitDashboard.svelte
+++ client/views/pages/pit-display/PitDashboard.svelte
@@ -1,4 +1,130 @@
 <script lang="ts">
+import { dateTime } from './../../../../shared/clock';
+import { match } from './../../../../shared/match';
+import { FIRSTEvent } from './../../../models/FIRST/event';
+import { FIRSTMatch } from './../../../models/FIRST/match';
+import { onMount } from 'svelte';
+import RobotCard from './RobotCard.svelte';
+
+let matchScouting: {
+    teams: {
+        team: number;
+        scouted: boolean;
+    }[];
+    match?: FIRSTMatch;
+}[] = [];
+
+let selectedMatchTime: number = 0; // Initialize with default time
+
+onMount(() => {
+    const fn = async (e: FIRSTEvent) => {
+        const [statusRes, matchesRes] = await Promise.all([
+            e.getStatus(),
+            e.getMatches()
+        ]);
+        if (statusRes.isErr()) return console.error(statusRes.error);
+        if (matchesRes.isErr()) return console.error(matchesRes.error);
+
+        const { matches } = statusRes.value;
+
+        matchScouting = await Promise.all(
+            matchesRes.value.map(async m => {
+                const match = matches.find(
+                    _m => _m.match === m.number && _m.compLevel === m.compLevel
+                );
+                if (!match) {
+                    return {
+                        teams: []
+                    };
+                }
+                const unScouted = match.teams;
+                const teams = await m.getTeams();
+
+                if (teams.isErr()) {
+                    return {
+                        teams: []
+                    };
+                }
+
+                return {
+                    teams: teams.value.filter(Boolean).map(t => ({
+                        team: t.number,
+                        scouted: !unScouted.includes(t.number)
+                    })),
+                    match: m
+                };
+            })
+        );
+    };
+
+    FIRSTEvent.on('select', fn);
+
+    if (FIRSTEvent.current) {
+        fn(FIRSTEvent.current);
+    }
+
+    return () => {
+        FIRSTEvent.off('select', fn);
+    };
+});
+
+function findClosestMatch(time: number) {
+    let closestMatch: FIRSTMatch | undefined;
+    let closestDiff = Infinity;
+
+    matchScouting.forEach(({ match }) => {
+        if (!match) return;
+
+        // Calculate the difference in time
+        const matchTime = match.tba.predicted_time;
+        const diff = Math.abs(matchTime - time);
+
+        // Update closest match if this one is closer
+        if (diff < closestDiff) {
+            closestDiff = diff;
+            closestMatch = match;
+        }
+    });
+
+    return closestMatch;
+}
+
+let red1 = 1;
+let red2 = 2;
+let red3 = 3;
+
+let blue1 = 4;
+let blue2 = 5;
+let blue3 = 6;
+
+let minutes = 0;
+
+function fetchTeamsData(match: FIRSTMatch) {
+    const teams = match.getTeams();
+    return teams.then(teamsResult => {
+        if (teamsResult.isErr()) {
+            console.error(teamsResult.error);
+            return [];
+        }
+        return teamsResult.value.map(team => team?.tba.team_number);
+    });
+}
+
+$: {
+    if (closestMatch) {
+        fetchTeamsData(closestMatch).then(teams => {
+            teams.forEach(team => {
+                console.log(team);
+            });
+        });
+    }
+}
+
+$: closestMatch = findClosestMatch(selectedMatchTime);
+$: matchnum = closestMatch?.number || 0;
+$: minutes = Math.floor((selectedMatchTime * 1000 - Date.now()) / 60000);
+$: console.log(selectedMatchTime * 1000, dateTime(selectedMatchTime * 1000));
+$: matchtime = dateTime(Number(closestMatch?.tba.predicted_time) * 1000);
 </script>
 
 <div class="vh-display vw-100 container-fluid position-relative">
@@ -7,52 +133,53 @@
             <div
                 class="d-flex flex-column h-100 justify-content-between align-items-start p-3"
             >
-                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
-                    <!-- Content 1 -->
-                </div>
-                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
-                    <!-- Content 2 -->
-                </div>
-                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
-                    <!-- Content 3 -->
-                </div>
+                <RobotCard robot="{red1}" {matchnum} />
+                <RobotCard robot="{red2}" {matchnum} />
+                <RobotCard robot="{red3}" {matchnum} />
             </div>
         </div>
         <div class="col-md-6 bg-primary">
             <div
                 class="d-flex flex-column h-100 justify-content-between align-items-end p-3"
             >
-                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
-                    <!-- Content 1 -->
-                </div>
-                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
-                    <!-- Content 2 -->
-                </div>
-                <div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
-                    <!-- Content 3 -->
-                </div>
+                <RobotCard robot="{blue1}" {matchnum} />
+                <RobotCard robot="{blue2}" {matchnum} />
+                <RobotCard robot="{blue3}" {matchnum} />
             </div>
         </div>
     </div>
     <!-- Dark gray square overlay -->
     <div
-        class="container h-25 w-25 bg-gray-light rounded position-absolute top-50 start-50 translate-middle z-3"
+        class="container h-50 w-25 bg-gray-light rounded position-absolute top-50 start-50 translate-middle z-3"
     >
         <div
             class="d-flex flex-column justify-content-center align-items-center h-100 my-2"
         >
-            <h4 class="text-black">Next match is in __ minutes at</h4>
-            <p class="display-1 text-black" style="font-size:900%">00:00</p>
+            <h4 class="text-black">
+                Match {matchnum} is in {minutes} minutes at
+            </h4>
+            <p class="display-1 text-black" style="font-size:400%">
+                {matchtime}
+            </p>
+            <select bind:value="{selectedMatchTime}">
+                {#each matchScouting as { match }}
+                    {#if match}
+                        <option value="{match.tba.predicted_time}"
+                            >{match.compLevel} {match.number}</option
+                        >
+                    {/if}
+                {/each}
+            </select>
         </div>
     </div>
 </div>
 
 <style>
-.bg-gray-light {
-    background-color: rgba(200, 200, 200, 0.7);
-}
-
 .vh-display {
     height: calc(100vh - 58px);
+}
+
+.bg-gray-light {
+    background-color: rgba(200, 200, 200, 0.7);
 }
 </style>
---
+++ client/views/pages/pit-display/RobotCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+export let robot: number;
+export let match: string;
+</script>
+
+<div class="mb-3 bg-gray-light flex-fill p-3 rounded w-75">
+    <div class="d-flex flex-column justify-content-between align-items-center">
+        <h4 class="text-black">Robot {robot}</h4>
+    </div>
+</div>
+
+<style>
+.bg-gray-light {
+    background-color: rgba(200, 200, 200, 0.7);
+}
+</style>
