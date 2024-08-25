be1e5bc2eebe92ee18713f9c4fd3fe59b342fc09
tsaxking <taylorking206@u.boisestate.edu>
2024-08-25 15:15:49 -0600

    change whitebaord db version

--- storage/db/queries/db/versions/1-4-4.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE Strategy DROP COLUMN whiteboardId;
-
-ALTER TABLE Whiteboards DROP COLUMN matchId;
-ALTER TABLE Whiteboards DROP COLUMN customMatchId;
-ALTER TABLE Whiteboards DROP COLUMN eventKey;
-ALTER TABLE Whiteboards ADD COLUMN strategyId TEXT NOT NULL;
-ALTER TABLE Whiteboards ADD COLUMN archived BOOLEAN NOT NULL;
