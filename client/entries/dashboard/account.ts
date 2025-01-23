0896b7388761a65220f84ba2cbb32c3e8e5254a1
Ansodo-SophieH <ansodohung@gmail.com>
2025-01-22 19:28:40 -0700

    deleted Account.svelte

--- client/entries/dashboard/account.ts
+++ client/entries/dashboard/account.ts
@@ -1,5 +1,5 @@
 import '../../utilities/imports';
-import App from '../../views/pages/account.svelte';
+import App from '../../views/pages/Account.svelte';
 import '../../models/FIRST/model-builder';
 import { FIRSTYear } from '../../models/FIRST/year';
 FIRSTYear.select(new Date().getFullYear());
